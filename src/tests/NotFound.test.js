542ee8baef37d9f60120451f9dc3b1ca0b35202c
Manuela França <manuelaalves26@gmail.com>
2022-07-18 15:11:10 -0300

    Requisito 3.

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+describe('Testando o NotFound.js', () => {
+  test('Testa se a pg contém um heading h2 com o text Page requested not found 😭', () => {
+    render(<NotFound />);
+    const textH2 = screen.getByText(/Page requested not found/i);
+    expect(textH2).toBeInTheDocument();
+  });
+
+  test('Testa se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    render(<NotFound />);
+    const image = screen.getByAltText(/Pikachu crying because the page/i);
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
